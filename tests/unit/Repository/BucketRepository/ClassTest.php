a4374b4e214a323d7699889bc43ab719ff8cd5a5
Stefan Koopmanschap <stefan.koopmanschap@tenzinger.com>
2024-05-27 13:08:10 +0200

    Added some extra testcases for the BucketRepository to increase test coverage

--- tests/unit/Repository/BucketRepository/ClassTest.php
+++ tests/unit/Repository/BucketRepository/ClassTest.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace BowlOfSoup\CouchbaseAccessLayer\Test\unit\Repository\BucketRepository;
 
 use BowlOfSoup\CouchbaseAccessLayer\Builder\QueryBuilder;
+use BowlOfSoup\CouchbaseAccessLayer\Factory\ClusterFactory;
+use BowlOfSoup\CouchbaseAccessLayer\Repository\BucketRepository;
 use BowlOfSoup\CouchbaseAccessLayer\Test\AbstractTest;
 use Couchbase\Bucket;
+use Couchbase\BucketInterface;
+use Couchbase\ClusterInterface;
 use Couchbase\Exception\CouchbaseException;
 use Couchbase\Exception\DocumentNotFoundException;
+use Couchbase\QueryOptions;
+use Couchbase\QueryResult;
 
 /**
  * Tests all other units from BucketRepository that where not tested in other test classes in this namespace.
@@ -70,5 +76,59 @@ class ClassTest extends AbstractTest
         $this->expectException(DocumentNotFoundException::class);
 
         $this->bucketRepository->remove('123abc');
+    }
+
+    public function testConstructorHandlesSituationWherePasswordIsNeeded(): void
+    {
+        $clusterFactory = $this->createMock(ClusterFactory::class);
+        $cluster = $this->createMock(ClusterInterface::class);
+        $bucket = $this->createMock(Bucket::class);
+
+        $cluster->expects($this->exactly(2))->method('bucket')->willReturnCallback(function($bucketName, $password = '') use ($bucket) {
+            if ($password === '') {
+                throw new \Exception('Test');
+            }
+
+            return $bucket;
+        });
+
+        $clusterFactory->expects($this->once())->method('create')->willReturn($cluster);
+
+        $bucketRepository = new BucketRepository('default', $clusterFactory, 'password');
+
+        self::assertInstanceOf(BucketInterface::class, $bucketRepository->getBucket());
+    }
+
+    public function testGetResultUnprocessedWithParametersPassesParametersCorrectly(): void
+    {
+        $clusterFactory = $this->createMock(ClusterFactory::class);
+        $cluster = $this->createMock(ClusterInterface::class);
+        $bucket = $this->createMock(Bucket::class);
+
+        $cluster->expects($this->once())->method('bucket')->with('default')->willReturn($bucket);
+
+        $clusterFactory->expects($this->once())->method('create')->willReturn($cluster);
+
+        $cluster->expects($this->once())->method('query')->willReturnCallback(function($query, QueryOptions $options) {
+            $export = QueryOptions::export($options);
+            self::assertArrayHasKey('parameter', $export['namedParameters']);
+
+            $resultMock = $this->createMock(QueryResult::class);
+            $resultMock->expects($this->once())->method('rows')->willReturn([['id' => 1337]]);
+
+            return $resultMock;
+        });
+
+        $bucketRepository = new BucketRepository('default', $clusterFactory);
+
+        $queryBuilder = new QueryBuilder();
+        $queryBuilder->from('default');
+        $queryBuilder->setParameters(['parameter' => 'value']);
+
+        $result = $bucketRepository->getResultUnprocessed($queryBuilder);
+
+        self::assertIsArray($result);
+        self::assertArrayHasKey('id', $result[0]);
+        self::assertSame(1337, $result[0]['id']);
     }
 }
